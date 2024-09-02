04e8953b95e4772682ca3233e97416e4926661e6
zozozrob <111177723+zozozrob@users.noreply.github.com>
2024-09-02 13:10:15 +0300

    Update CATESEIDHOLA.py

--- CATESEIDHOLA.py
+++ CATESEIDHOLA.py
@@ -1,5 +1,33 @@
-x == int(input("HOLA, entir words yas or nyo"))
-if x==yas:
-  print("Congratulatiton!!!")
-else:
-  print("bruh")
+from tkinter import *
+
+class Main(Frame):
+    def __init__(self, root):
+        super(Main, self).__init__(root)
+        self.build()
+
+    def build(self):
+        pass
+ 
+    def logicalc(self, operation):
+        pass
+
+    def update():
+       pass
+
+
+if __name__ == '__main__':
+    root = Tk()
+    root["bg"] = "#000"
+    root.geometry("485x550+200+200")
+    root.title("Калькулятор")
+    root.resizable(False, False)
+    app = Main(root)
+    app.pack()
+    root.mainloop()
+btns = [
+            "C", "DEL", "*", "=",
+            "1", "2", "3", "/",
+            "4", "5", "6", "+",
+            "7", "8", "9", "-",
+            "+/-", "0", "%", "X^2"
+        ]
